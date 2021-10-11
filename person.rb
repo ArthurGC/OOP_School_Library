dcf7d218580a966a40b6b2e3b730872530ac6d1a
arthurgc <alxguzmanc@gmail.com>
2021-10-11 17:36:34 -0500

    Add getters and setters in Person class

--- person.rb
+++ person.rb
@@ -1,4 +1,7 @@
 class Person
+    attr_accessor :name
+    attr_accessor :age
+    attr_reader :id
 
     def initialize(age, name = "Unknown", parent_permission = true)
         @id = Random.rand(1..1000)
