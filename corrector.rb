c5fec1ecae717328c636cd4056f34b5b2ef333d6
arthurgc <alxguzmanc@gmail.com>
2021-10-12 14:39:12 -0500

    Add correct_name method

--- corrector.rb
+++ corrector.rb
@@ -1,3 +1,11 @@
 class Corrector
-    
+  def correct_name(name)
+    if name.length > 10
+      name.capitalize.strip
+    else
+      name.capitalize
+    end
+  end
 end
+
+p Corrector.new.correct_name('alexanderDrigoasdasdas')
