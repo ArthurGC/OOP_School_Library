86b5bc9af4c00dc5bedc9a2a105058ad942eada5
arthurgc <alxguzmanc@gmail.com>
2021-10-14 13:45:22 -0500

    Add books, people, and rentals instance variables in App class

--- app.rb
+++ app.rb
@@ -1,3 +1,10 @@
 class App
-    
+    attr_reader :books, :people, :rentals
+
+    initialize(books = [], people = [], rentals = [] )
+    @books = books
+    @people = people
+    @rentals = rentals 
+end
+
 end
