c22d53919f77de0cd7da36365759e088d2da8392
arthurgc <alxguzmanc@gmail.com>
2021-10-14 13:41:21 -0500

    Add list of options in main method

--- main.rb
+++ main.rb
@@ -1,5 +1,38 @@
-class main
-    
+def main
+    status = true
+    while status
+      puts 'Welcome to School Library App!'
+      puts
+      puts 'Please choose an option by entering a number:'
+      puts '1 - List all books'
+      puts '2 - List all people'
+      puts '3 - Create a person'
+      puts '4 - Create a book'
+      puts '5 - Create a rental'
+      puts '6 - List all rentals for a given person id'
+      puts '7 - Exit'
+  
+      option = gets.chomp
+  
+      case option
+      when '1'
+        app.all_books
+      when '2'
+        app.all_people
+      when '3'
+        app.create_person
+      when '4'
+        app.create_book
+      when '5'
+        app.create_rental
+      when '6'
+        app.all_rentals_id
+      when '7'
+        status = false
+      else
+        puts 'Sorry, you choose a wrong option'
+      end
+    end
 end
 
 main
