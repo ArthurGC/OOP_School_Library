4f5f5221ea171af2c61bbeaaf64fac8126764d0d
arthurgc <alxguzmanc@gmail.com>
2021-10-14 15:31:03 -0500

    Add create_book method

--- app.rb
+++ app.rb
@@ -60,4 +60,15 @@ class App
         @people << person
         puts "Person created successfully"
   end
+
+  def create_book
+      print "Title: "
+      title = gets.chomp
+      print "Author: "
+      author = gets.chomp
+
+      book = Book.new(title, author)
+      @books << book
+      puts "Book created successfully"
+  end
 end
