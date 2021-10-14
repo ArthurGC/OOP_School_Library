244cb6226d59d3df66a34879c4819d564d2f0617
arthurgc <alxguzmanc@gmail.com>
2021-10-14 13:59:12 -0500

    Add all_people method

--- app.rb
+++ app.rb
@@ -10,4 +10,8 @@ class App
   def all_books
     @books.each {|book| puts "Title: #{book.title}, Author: #{book.author}"}
   end
+
+  def all_people
+    @people.each {|person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"}
+  end
 end
