280f8e76554188ef10d72f80726611aa6248d150
arthurgc <alxguzmanc@gmail.com>
2021-10-14 13:54:06 -0500

    Add all_books method

--- app.rb
+++ app.rb
@@ -1,10 +1,13 @@
 class App
-    attr_reader :books, :people, :rentals
+  attr_reader :books, :people, :rentals
 
-    initialize(books = [], people = [], rentals = [] )
+  def initialize(books = [], people = [], rentals = [])
     @books = books
     @people = people
-    @rentals = rentals 
-end
+    @rentals = rentals
+  end
 
+  def all_books
+    @books.each {|book| puts "Title: #{book.title}, Author: #{book.author}"}
+  end
 end
--- main.rb
+++ main.rb
@@ -1,39 +1,39 @@
 def main
-    app = App.new
-    status = true
-    while status
-      puts 'Welcome to School Library App!'
-      puts
-      puts 'Please choose an option by entering a number:'
-      puts '1 - List all books'
-      puts '2 - List all people'
-      puts '3 - Create a person'
-      puts '4 - Create a book'
-      puts '5 - Create a rental'
-      puts '6 - List all rentals for a given person id'
-      puts '7 - Exit'
-  
-      option = gets.chomp
-  
-      case option
-      when '1'
-        app.all_books
-      when '2'
-        app.all_people
-      when '3'
-        app.create_person
-      when '4'
-        app.create_book
-      when '5'
-        app.create_rental
-      when '6'
-        app.all_rentals_id
-      when '7'
-        status = false
-      else
-        puts 'Sorry, you choose a wrong option'
-      end
+  app = App.new
+  status = true
+  while status
+    puts 'Welcome to School Library App!'
+    puts
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+
+    option = gets.chomp
+
+    case option
+    when '1'
+      app.all_books
+    when '2'
+      app.all_people
+    when '3'
+      app.create_person
+    when '4'
+      app.create_book
+    when '5'
+      app.create_rental
+    when '6'
+      app.all_rentals_id
+    when '7'
+      status = false
+    else
+      puts 'Sorry, you choose a wrong option'
     end
+  end
 end
 
 main
