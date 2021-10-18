e6a8cf4d00a51e89006a35db01161efaea781a45
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-10-18 18:59:51 -0300

    Refactor classes

--- main.rb
+++ main.rb
@@ -1,10 +1,9 @@
 require_relative './app'
 
 def main
-  app = App.new
-  puts 'Welcome to School Library App!'
-
-
+  app = App.new  
+  puts 'Welcome to School Library App!'  
+  app.ui_init
 end
 
 main
