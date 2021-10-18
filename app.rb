3882752b74c3e6c5555a84e017a6c1bda7aa5f49
arthurgc <alxguzmanc@gmail.com>
2021-10-18 14:44:17 -0500

    Add ui_init method

--- app.rb
+++ app.rb
@@ -12,6 +12,20 @@ class App
     @rentals = rentals
   end
 
+  def ui_init
+    puts "\nPlease choose an option by entering a number:"
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+
+    option = gets.chomp.to_i
+    check_options(option)
+  end
+
   def all_books
     @books.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
   end
